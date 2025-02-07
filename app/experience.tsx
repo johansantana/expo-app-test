8d86ca53928820812efea1c86e38c411a140248e
Johan Santana <johancarlosst@gmail.com>
2025-02-07 00:39:57 -0400

    feat: modify experience

--- app/experience.tsx
+++ app/experience.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from 'react-native'
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 export default function Experience() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <WebView
+        style={styles.webview}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        source={{ uri: 'https://youtu.be/gv6rn8hTyH0' }}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  webview: {
+    width: '100%',
+    height: '100%'
+  }
+})
